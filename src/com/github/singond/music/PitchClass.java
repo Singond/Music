4c3e73e2c37576a52c19d14a26c8b32b58ebfea5
Jan Singon Slany <slanyj@centrum.cz>
2018-01-25 20:20:57 +0100

    Add javadoc and enharmonicity comparison

--- src/com/github/singond/music/PitchClass.java
+++ src/com/github/singond/music/PitchClass.java
@@ -4,20 +4,56 @@ public class PitchClass {
 
 	private final BasePitchClass base;
 	private final Accidental accidental;
-	
+
 	private PitchClass(BasePitchClass base, Accidental accidental) {
 		this.base = base;
 		this.accidental = accidental;
 	}
-	
-	public BasePitchClass getBasePitchClass() {
+
+	public static PitchClass of(BasePitchClass base, Accidental accidental) {
+		return new PitchClass(base, accidental);
+	}
+
+	/**
+	 * Returns the natural form of this pitch class,
+	 * e.g. for "C#" returns "C".
+	 * @return the natural of this pitch class
+	 */
+	public BasePitchClass naturalPitchClass() {
 		return base;
 	}
-	
-	public Accidental getAccidental() {
+
+	/**
+	 * Returns the accidental of this pitch class,
+	 * that is, its relation to the natural.
+	 * @return the accidental
+	 */
+	public Accidental accidental() {
 		return accidental;
 	}
-	
+
+	/**
+	 * Returns the number of semitone steps of this pitch class above the
+	 * reference pitch class, which is C.
+	 * @return number of semitones above C
+	 */
+	public int stepsAboveReference() {
+		return base.stepsAboveReference() + accidental.stepsAboveNatural();
+	}
+
+	/**
+	 * Checks whether the given pitch class is the same number of semitones
+	 * above the reference as this pitch class, disregarding any octave
+	 * differences.
+	 *
+	 * @param other the pitch class to be compared with this one
+	 * @return {@code true} if the difference of the semitones above
+	 *         reference of the two pitch classes is a multiple of 12
+	 */
+	public boolean isEnharmonicWith(PitchClass other) {
+		return (stepsAboveReference() - other.stepsAboveReference()) % 12 == 0;
+	}
+
 	@Override
 	public String toString() {
 		return base + accidental.symbolAscii();
