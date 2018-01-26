54c6d8e1c543c57055aa0e89cea8a2580a44a36f
Jan Singon Slany <slanyj@centrum.cz>
2018-01-26 19:29:12 +0100

    Implement intervals

---
+++ src/com/github/singond/music/Interval.java
@@ -0,0 +1,60 @@
+package com.github.singond.music;
+
+/**
+ * An absolute distance of two pitches.
+ * This interval object holds no information of the direction in which
+ * the interval is applied, it is merely the distance of the two pitches,
+ * expressed as an absolute value.
+ * This definition prohibits pitches with negative number of semitones,
+ * like what is sometimes called the "diminished unison".
+ * 
+ *
+ * @author Singon
+ */
+public interface Interval {
+
+	/**
+	 * Returns the number of diatonic degrees spanned by this interval,
+	 * including the start degree but not the end degree.
+	 * If the start degree is {@code a} and the end degree is {@code b},
+	 * this method returns the number {@code b-a}.
+	 * Alternatively, this may be thought of as the number of staff
+	 * positions between the two pitches, excluding the end pitch.
+	 * <p>
+	 * <strong>CAUTION:</strong> This is contrary to the naming convention
+	 * of intervals, where both the start degree and end degree are included.
+	 * For example, for any interval called "a third" (be it major, minor,
+	 * diminished or augmented), this method returns the number <em>2</em>.
+	 * <p>
+	 * For example, for the interval from C4 to G4, returns 4;
+	 * for the interval from C#4 to G4 returns 4 as well;
+	 * but for the interval from Db to G4 returns 3.
+	 *
+	 * @return the number of diatonic scale degrees including start and end
+	 */
+	int degrees();
+	
+	/**
+	 * Returns the absolute width of the interval in semitones.
+	 * The width of an interval is the absolute value of the number
+	 * of semitone steps between the start and end pitch of the interval.
+	 * This implies that the returned value must not be negative.
+	 * <p>
+	 * For example, for the interval from C4 to G4, returns 7;
+	 * for the interval from C#4 to G4 returns 6;
+	 * and for the interval from Db to G4 returns 6 as well.
+	 *
+	 * @return the width of the interval in semitones (always a positive number)
+	 */
+	int semitones();
+	
+	/**
+	 * Returns the number of diatonic degrees spanned by this interval,
+	 * including the start degree and the end degree.
+	 * This is the number which forms the base of the name of the interval,
+	 * and must be equivalent to calling {@code degrees() + 1}.
+	 *
+	 * @return the interval number. For example, for a major third, returns 3.
+	 */
+	int intervalNumber();
+}
---
+++ src/com/github/singond/music/SimpleInterval.java
@@ -0,0 +1,89 @@
+package com.github.singond.music;
+
+/**
+ * The set of intervals spanning from one to eight degrees,
+ * that is, from unison to octave.
+ * The width of these intervals does not exceed 12 steps (a perfect octave),
+ * with one exception: There is augmented octave, which spans 13 semitones.
+ *
+ * @author Singon
+ */
+public enum SimpleInterval implements Interval {
+
+	// Basic
+	UNISON              (0, 0,  "unison",               "P1"),
+	MINOR_SECOND        (1, 1,  "minor second",         "m2"),
+	MAJOR_SECOND        (1, 2,  "major second",         "M2"),
+	MINOR_THIRD         (2, 3,  "minor third",          "m3"),
+	MAJOR_THIRD         (2, 4,  "major third",          "M3"),
+	PERFECT_FOURTH      (3, 5,  "perfect fourth",       "P4"),
+	PERFECT_FIFTH       (4, 7,  "perfect fifth",        "P5"),
+	MINOR_SIXTH         (5, 8,  "minor sixth",          "m6"),
+	MAJOR_SIXTH         (5, 9,  "major sixth",          "M6"),
+	MINOR_SEVENTH       (6, 10, "minor seventh",        "m7"),
+	MAJOR_SEVENTH       (6, 11, "major seventh",        "M7"),
+	PERFECT_OCTAVE      (7, 12, "perfect octave",       "P8"),
+	
+	// Extended
+	AUGMENTED_UNISON    (0, 1,  "augmented unison",     "A1"),
+	DIMINISHED_SECOND   (1, 0,  "diminished second",    "d2"),
+	AUGMENTED_SECOND    (1, 3,  "augmented second",     "A2"),
+	DIMINISHED_THIRD    (2, 2,  "diminished third",     "d3"),
+	AUGMENTED_THIRD     (2, 5,  "augmented third",      "A3"),
+	DIMINISHED_FOURTH   (3, 4,  "diminished fourth",    "d4"),
+	AUGMENTED_FOURTH    (3, 6,  "augmented fourth",     "A4"),
+	DIMINISHED_FIFTH    (4, 6,  "diminished fifth",     "d5"),
+	AUGMENTED_FIFTH     (4, 8,  "augmented fifth",      "A5"),
+	DIMINISHED_SIXTH    (5, 7,  "diminished sixth",     "d6"),
+	AUGMENTED_SIXTH     (5, 10, "augmented sixth",      "A6"),
+	DIMINISHED_SEVENTH  (6, 9,  "diminished seventh",   "d7"),
+	AUGMENTED_SEVENTH   (6, 12, "augmented seventh",    "A7"),
+	DIMINISHED_OCTAVE   (7, 11, "diminished octave",    "d8"),
+	AUGMENTED_OCTAVE    (7, 13, "augmented octave",     "A8");
+
+	
+	private final int degrees;
+	private final int semitones;
+	private final String name;
+	private final String symbol;
+	
+	/**
+	 * @param degrees the number of degree steps spanned
+	 * @param width the width in semitones
+	 */
+	private SimpleInterval(int degrees, int semitones,
+	                       String name, String symbol) {
+		this.degrees = degrees;
+		this.semitones = semitones;
+		this.name = name;
+		this.symbol = symbol;
+	}
+
+	@Override
+	public int degrees() {
+		return degrees;
+	}
+
+	@Override
+	public int semitones() {
+		return semitones;
+	}
+
+	@Override
+	public int intervalNumber() {
+		return degrees + 1;
+	}
+
+	public String longName() {
+		return name;
+	}
+
+	public String symbol() {
+		return symbol;
+	}
+
+	@Override
+	public String toString() {
+		return symbol;
+	}
+}
