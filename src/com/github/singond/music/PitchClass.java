9c0296a4f406ac8ae2e3f387b249127a09b04ce7
Jan Singon Slany <slanyj@centrum.cz>
2018-01-27 10:35:27 +0100

    Cache instances of PitchClass

--- src/com/github/singond/music/PitchClass.java
+++ src/com/github/singond/music/PitchClass.java
@@ -1,5 +1,11 @@
 package com.github.singond.music;
 
+import static com.github.singond.music.Accidental.*;
+import static com.github.singond.music.BasePitchClass.*;
+
+import java.util.EnumMap;
+import java.util.Map;
+
 /**
  * A set of all pitches which are a perfect octave apart.
  * <p>
@@ -13,14 +19,87 @@ public class PitchClass {
 	private final Accidental accidental;
 	
 	private static final int SEMITONES = 12;
+	
+	private static final Map<BasePitchClass, PitchClass> doubleFlats;
+	private static final Map<BasePitchClass, PitchClass> flats;
+	private static final Map<BasePitchClass, PitchClass> naturals;
+	private static final Map<BasePitchClass, PitchClass> sharps;
+	private static final Map<BasePitchClass, PitchClass> doubleSharps;
+	static {
+		doubleFlats = new EnumMap<>(BasePitchClass.class);
+		doubleFlats.put(C, new PitchClass(C, DOUBLE_FLAT));
+		doubleFlats.put(D, new PitchClass(D, DOUBLE_FLAT));
+		doubleFlats.put(E, new PitchClass(E, DOUBLE_FLAT));
+		doubleFlats.put(F, new PitchClass(F, DOUBLE_FLAT));
+		doubleFlats.put(G, new PitchClass(G, DOUBLE_FLAT));
+		doubleFlats.put(A, new PitchClass(A, DOUBLE_FLAT));
+		doubleFlats.put(B, new PitchClass(B, DOUBLE_FLAT));
+		
+		flats = new EnumMap<>(BasePitchClass.class);
+		flats.put(C, new PitchClass(C, FLAT));
+		flats.put(D, new PitchClass(D, FLAT));
+		flats.put(E, new PitchClass(E, FLAT));
+		flats.put(F, new PitchClass(F, FLAT));
+		flats.put(G, new PitchClass(G, FLAT));
+		flats.put(A, new PitchClass(A, FLAT));
+		flats.put(B, new PitchClass(B, FLAT));
+		
+		naturals = new EnumMap<>(BasePitchClass.class);
+		naturals.put(C, new PitchClass(C, NATURAL));
+		naturals.put(D, new PitchClass(D, NATURAL));
+		naturals.put(E, new PitchClass(E, NATURAL));
+		naturals.put(F, new PitchClass(F, NATURAL));
+		naturals.put(G, new PitchClass(G, NATURAL));
+		naturals.put(A, new PitchClass(A, NATURAL));
+		naturals.put(B, new PitchClass(B, NATURAL));
+		
+		sharps = new EnumMap<>(BasePitchClass.class);
+		sharps.put(C, new PitchClass(C, SHARP));
+		sharps.put(D, new PitchClass(D, SHARP));
+		sharps.put(E, new PitchClass(E, SHARP));
+		sharps.put(F, new PitchClass(F, SHARP));
+		sharps.put(G, new PitchClass(G, SHARP));
+		sharps.put(A, new PitchClass(A, SHARP));
+		sharps.put(B, new PitchClass(B, SHARP));
+		
+		doubleSharps = new EnumMap<>(BasePitchClass.class);
+		doubleSharps.put(C, new PitchClass(C, DOUBLE_SHARP));
+		doubleSharps.put(D, new PitchClass(D, DOUBLE_SHARP));
+		doubleSharps.put(E, new PitchClass(E, DOUBLE_SHARP));
+		doubleSharps.put(F, new PitchClass(F, DOUBLE_SHARP));
+		doubleSharps.put(G, new PitchClass(G, DOUBLE_SHARP));
+		doubleSharps.put(A, new PitchClass(A, DOUBLE_SHARP));
+		doubleSharps.put(B, new PitchClass(B, DOUBLE_SHARP));
+	}
 
 	private PitchClass(BasePitchClass base, Accidental accidental) {
 		this.base = base;
 		this.accidental = accidental;
 	}
 
+	/**
+	 * Returns the pitch class of the given natural pitch class and accidental.
+	 * This factory always returns a cached result for the pitches with
+	 * accidentals ranging from double flat to double sharp (inclusive).
+	 *
+	 * @param base the natural pitch class (e.g. {@code C} for {@code C#})
+	 * @param accidental the accidental relating the pitch class to the natural
+	 * @return
+	 */
 	public static PitchClass of(BasePitchClass base, Accidental accidental) {
-		return new PitchClass(base, accidental);
+		if (accidental.equals(DOUBLE_FLAT)) {
+			return doubleFlats.get(base);
+		} else if (accidental.equals(FLAT)) {
+			return flats.get(base);
+		} else if (accidental.equals(NATURAL)) {
+			return naturals.get(base);
+		} else if (accidental.equals(SHARP)) {
+			return sharps.get(base);
+		} else if (accidental.equals(DOUBLE_SHARP)) {
+			return doubleSharps.get(base);
+		} else {
+			return new PitchClass(base, accidental);
+		}
 	}
 
 	/**
