0fce1225e4a1c172dc7c2dfcabd8de715c575c31
Jan Singon Slany <slanyj@centrum.cz>
2018-05-20 13:03:40 +0200

    Correct issues in Interval javadoc

--- src/com/github/singond/music/Interval.java
+++ src/com/github/singond/music/Interval.java
@@ -6,7 +6,7 @@ package com.github.singond.music;
  * the interval is applied, it is merely the distance of the two pitches,
  * expressed as an absolute value.
  * This definition prohibits pitches with negative number of semitones,
- * like what is sometimes called the "diminished unison".
+ * such as what is sometimes called the "diminished unison".
  *
  * @author Singon
  */
@@ -17,19 +17,22 @@ public interface Interval {
 	 * including the start degree but not the end degree.
 	 * If the start degree is {@code a} and the end degree is {@code b},
 	 * this method returns the number {@code b-a}.
-	 * Alternatively, this may be thought of as the number of staff
-	 * positions between the two pitches, excluding the end pitch.
+	 * <p>
+	 * When visualized on staff, this is the number of steps a notehead
+	 * must make (from a line to the adjacent space or from a space to the
+	 * adjacent line) when moving from the start pitch to the end pitch.
 	 * <p>
 	 * <strong>CAUTION:</strong> This is contrary to the naming convention
 	 * of intervals, where both the start degree and end degree are included.
 	 * For example, for any interval called "a third" (be it major, minor,
 	 * diminished or augmented), this method returns the number <em>2</em>.
 	 * <p>
-	 * For example, for the interval from C4 to G4, returns 4;
-	 * for the interval from C#4 to G4 returns 4 as well;
-	 * but for the interval from Db to G4 returns 3.
+	 * For example, for the interval from C4 to G4, this method returns 4;
+	 * for the interval from C#4 to G4 it returns 4 as well;
+	 * but for the interval from Db to G4 it returns 3.
 	 *
-	 * @return the number of diatonic scale degrees including start and end
+	 * @return the number of diatonic scale degrees including the start
+	 *         but not the end degree
 	 */
 	int degrees();
 	
@@ -39,21 +42,23 @@ public interface Interval {
 	 * of semitone steps between the start and end pitch of the interval.
 	 * This implies that the returned value must not be negative.
 	 * <p>
-	 * For example, for the interval from C4 to G4, returns 7;
-	 * for the interval from C#4 to G4 returns 6;
-	 * and for the interval from Db to G4 returns 6 as well.
+	 * For example, for the interval from C4 to G4, this method returns 7;
+	 * for the interval from C#4 to G4 it returns 6;
+	 * and for the interval from Db to G4 it returns 6 as well.
 	 *
-	 * @return the width of the interval in semitones (always a positive number)
+	 * @return the width of the interval in semitones (always positive)
 	 */
 	int semitones();
 	
 	/**
 	 * Returns the number of diatonic degrees spanned by this interval,
 	 * including the start degree and the end degree.
-	 * This is the number which forms the base of the name of the interval,
-	 * and must be equivalent to calling {@code degrees() + 1}.
+	 * This is the number which forms the base of the Latin name of the
+	 * interval, and is equivalent to {@code degrees() + 1}.
+	 * <p>
+	 * As an example, for a major third, this method returns 3.
 	 *
-	 * @return the interval number. For example, for a major third, returns 3.
+	 * @return the number of diatonic degrees including start and end degree
 	 */
 	int intervalNumber();
 	
