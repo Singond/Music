d0e867c8e7df813cb7f8e23342f4df3aaa5ddbcb
Jan Singon Slany <slanyj@centrum.cz>
2018-06-05 00:48:22 +0200

    Expose common keys as constants

--- src/com/github/singond/music/MajorKey.java
+++ src/com/github/singond/music/MajorKey.java
@@ -1,5 +1,7 @@
 package com.github.singond.music;
 
+import static com.github.singond.music.PitchClass.*;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +19,22 @@ public class MajorKey extends SimpleKey implements Key {
 			SimpleInterval.MAJOR_SECOND,
 			SimpleInterval.MAJOR_SECOND,
 			SimpleInterval.MAJOR_SECOND);
+	
+	public static final MajorKey C_FLAT_MAJOR = new MajorKey(C_FLAT);
+	public static final MajorKey G_FLAT_MAJOR = new MajorKey(G_FLAT);
+	public static final MajorKey D_FLAT_MAJOR = new MajorKey(D_FLAT);
+	public static final MajorKey A_FLAT_MAJOR = new MajorKey(A_FLAT);
+	public static final MajorKey E_FLAT_MAJOR = new MajorKey(E_FLAT);
+	public static final MajorKey B_FLAT_MAJOR = new MajorKey(B_FLAT);
+	public static final MajorKey F_MAJOR = new MajorKey(F);
+	public static final MajorKey C_MAJOR = new MajorKey(C);
+	public static final MajorKey G_MAJOR = new MajorKey(G);
+	public static final MajorKey D_MAJOR = new MajorKey(D);
+	public static final MajorKey A_MAJOR = new MajorKey(A);
+	public static final MajorKey E_MAJOR = new MajorKey(E);
+	public static final MajorKey B_MAJOR = new MajorKey(B);
+	public static final MajorKey F_SHARP_MAJOR = new MajorKey(F_SHARP);
+	public static final MajorKey C_SHARP_MAJOR = new MajorKey(C_SHARP);
 	
 	public MajorKey(PitchClass tonic) {
 		super(tonic, INTERVALS);
--- src/com/github/singond/music/MinorKey.java
+++ src/com/github/singond/music/MinorKey.java
@@ -1,5 +1,7 @@
 package com.github.singond.music;
 
+import static com.github.singond.music.PitchClass.*;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +19,22 @@ public class MinorKey extends SimpleKey implements Key {
 			SimpleInterval.MAJOR_SECOND,
 			SimpleInterval.MINOR_SECOND,
 			SimpleInterval.MAJOR_SECOND);
+	
+	public static final MinorKey A_FLAT_MINOR = new MinorKey(A_FLAT);
+	public static final MinorKey E_FLAT_MINOR = new MinorKey(E_FLAT);
+	public static final MinorKey B_FLAT_MINOR = new MinorKey(B_FLAT);
+	public static final MinorKey F_MINOR = new MinorKey(F);
+	public static final MinorKey C_MINOR = new MinorKey(C);
+	public static final MinorKey G_MINOR = new MinorKey(G);
+	public static final MinorKey D_MINOR = new MinorKey(D);
+	public static final MinorKey A_MINOR = new MinorKey(A);
+	public static final MinorKey E_MINOR = new MinorKey(E);
+	public static final MinorKey B_MINOR = new MinorKey(B);
+	public static final MinorKey F_SHARP_MINOR = new MinorKey(F_SHARP);
+	public static final MinorKey C_SHARP_MINOR = new MinorKey(C_SHARP);
+	public static final MinorKey G_SHARP_MINOR = new MinorKey(G_SHARP);
+	public static final MinorKey D_SHARP_MINOR = new MinorKey(D_SHARP);
+	public static final MinorKey A_SHARP_MINOR = new MinorKey(A_SHARP);
 	
 	public MinorKey(PitchClass tonic) {
 		super(tonic, INTERVALS);
