4914c3d849c2f6da1bd8f13800d9d3735a123442
Jan Singon Slany <slanyj@centrum.cz>
2018-07-11 23:24:03 +0200

    Specify equals() and compareTo() in Interval
    This requires SimpleInterval to be turned from enum into proper class.

--- src/com/github/singond/music/Interval.java
+++ src/com/github/singond/music/Interval.java
@@ -8,9 +8,15 @@ package com.github.singond.music;
  * This definition prohibits pitches with negative number of semitones,
  * such as what is sometimes called the "diminished unison".
  *
+ * This interface imposes a natural ordering on its implementations.
+ * This order sorts the intervals with lower width in semitones before
+ * the the wider intervals. If two intervals happen to have the same width,
+ * that with higher number of diatonic degrees is sorted last.
+ * This natural ordering is consisten with {@code equals}.
+ *
  * @author Singon
  */
-public interface Interval {
+public interface Interval extends Comparable<Interval> {
 
 	/**
 	 * Returns the number of diatonic degrees spanned by this interval,
@@ -35,7 +41,7 @@ public interface Interval {
 	 *         but not the end degree
 	 */
 	int degrees();
-	
+
 	/**
 	 * Returns the absolute width of the interval in semitones.
 	 * The width of an interval is the absolute value of the number
@@ -49,7 +55,7 @@ public interface Interval {
 	 * @return the width of the interval in semitones (always positive)
 	 */
 	int semitones();
-	
+
 	/**
 	 * Returns the number of diatonic degrees spanned by this interval,
 	 * including the start degree and the end degree.
@@ -61,7 +67,24 @@ public interface Interval {
 	 * @return the number of diatonic degrees including start and end degree
 	 */
 	int intervalNumber();
-	
+
+	/**
+	 * Compares another interval to this one.
+	 * This method first orders the intervals from smallest to largest
+	 * and then further orders enharmonic intervals in the order given
+	 * by their number of diatonic degrees, so that for example augmented
+	 * third is sorted before perfect fourth.
+	 * <p>
+	 * This method must throw {@code NullPointerException} when invoked
+	 * with null argument.
+	 *
+	 * @param i {@inheritDoc}
+	 * @return  {@inheritDoc}
+	 * @throws  NullPointerException {@inheritDoc}
+	 */
+	@Override
+	public int compareTo(Interval i);
+
 	/**
 	 * Indicates whether a given interval is enharmonic with this interval,
 	 * that is, if it spans the same number of semitones.
@@ -74,4 +97,21 @@ public interface Interval {
 	 *         as {@code other}
 	 */
 	boolean isEnharmonicWith(Interval other);
+
+	/**
+	 * Indicates whether a given object is equal to this interval, that is,
+	 * if the given object is also an interval and spans the same number
+	 * of semitones and the same number of diatonic degrees.
+	 * Thus, two intervals with the same width in semitones do not necessarily
+	 * be equal to each other.
+	 * For example, augmented third is not equal to perfect fourth, despite
+	 * the fact that both of these intervals have the same width.
+	 *
+	 * @param obj {@inheritDoc}
+	 * @return {@code true} if {@code obj} is also an {@code Interval} and if
+	 *         both {@code this} and {@code obj} have equal number of semitones
+	 *         and equal number of diatonic degrees
+	 */
+	@Override
+	boolean equals(Object object);
 }
--- src/com/github/singond/music/SimpleInterval.java
+++ src/com/github/singond/music/SimpleInterval.java
@@ -1,5 +1,8 @@
 package com.github.singond.music;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * The set of intervals spanning from one to eight degrees,
  * that is, from unison to octave.
@@ -8,45 +11,72 @@ package com.github.singond.music;
  *
  * @author Singon
  */
-public enum SimpleInterval implements Interval {
+public final class SimpleInterval extends AbstractInterval implements Interval {
 
-	// Basic
-	UNISON              (0, 0,  "unison",               "P1"),
-	MINOR_SECOND        (1, 1,  "minor second",         "m2"),
-	MAJOR_SECOND        (1, 2,  "major second",         "M2"),
-	MINOR_THIRD         (2, 3,  "minor third",          "m3"),
-	MAJOR_THIRD         (2, 4,  "major third",          "M3"),
-	PERFECT_FOURTH      (3, 5,  "perfect fourth",       "P4"),
-	PERFECT_FIFTH       (4, 7,  "perfect fifth",        "P5"),
-	MINOR_SIXTH         (5, 8,  "minor sixth",          "m6"),
-	MAJOR_SIXTH         (5, 9,  "major sixth",          "M6"),
-	MINOR_SEVENTH       (6, 10, "minor seventh",        "m7"),
-	MAJOR_SEVENTH       (6, 11, "major seventh",        "M7"),
-	PERFECT_OCTAVE      (7, 12, "perfect octave",       "P8"),
-	
-	// Extended
-	AUGMENTED_UNISON    (0, 1,  "augmented unison",     "A1"),
-	DIMINISHED_SECOND   (1, 0,  "diminished second",    "d2"),
-	AUGMENTED_SECOND    (1, 3,  "augmented second",     "A2"),
-	DIMINISHED_THIRD    (2, 2,  "diminished third",     "d3"),
-	AUGMENTED_THIRD     (2, 5,  "augmented third",      "A3"),
-	DIMINISHED_FOURTH   (3, 4,  "diminished fourth",    "d4"),
-	AUGMENTED_FOURTH    (3, 6,  "augmented fourth",     "A4"),
-	DIMINISHED_FIFTH    (4, 6,  "diminished fifth",     "d5"),
-	AUGMENTED_FIFTH     (4, 8,  "augmented fifth",      "A5"),
-	DIMINISHED_SIXTH    (5, 7,  "diminished sixth",     "d6"),
-	AUGMENTED_SIXTH     (5, 10, "augmented sixth",      "A6"),
-	DIMINISHED_SEVENTH  (6, 9,  "diminished seventh",   "d7"),
-	AUGMENTED_SEVENTH   (6, 12, "augmented seventh",    "A7"),
-	DIMINISHED_OCTAVE   (7, 11, "diminished octave",    "d8"),
-	AUGMENTED_OCTAVE    (7, 13, "augmented octave",     "A8");
+	/** Diatonic interval */
+	public static final SimpleInterval UNISON, MINOR_SECOND, MAJOR_SECOND,
+			MINOR_THIRD, MAJOR_THIRD, PERFECT_FOURTH, PERFECT_FIFTH,
+			MINOR_SIXTH, MAJOR_SIXTH, MINOR_SEVENTH, MAJOR_SEVENTH,
+			PERFECT_OCTAVE;
+	/** Extended interval */
+	public static final SimpleInterval AUGMENTED_UNISON,
+			DIMINISHED_SECOND, AUGMENTED_SECOND,
+			DIMINISHED_THIRD, AUGMENTED_THIRD,
+			DIMINISHED_FOURTH, AUGMENTED_FOURTH,
+			DIMINISHED_FIFTH, AUGMENTED_FIFTH,
+			DIMINISHED_SIXTH, AUGMENTED_SIXTH,
+			DIMINISHED_SEVENTH, AUGMENTED_SEVENTH,
+			DIMINISHED_OCTAVE, AUGMENTED_OCTAVE;
+	static {
+		UNISON             = new SimpleInterval(0, 0,  "unison",             "P1");
+		MINOR_SECOND       = new SimpleInterval(1, 1,  "minor second",       "m2");
+		MAJOR_SECOND       = new SimpleInterval(1, 2,  "major second",       "M2");
+		MINOR_THIRD        = new SimpleInterval(2, 3,  "minor third",        "m3");
+		MAJOR_THIRD        = new SimpleInterval(2, 4,  "major third",        "M3");
+		PERFECT_FOURTH     = new SimpleInterval(3, 5,  "perfect fourth",     "P4");
+		PERFECT_FIFTH      = new SimpleInterval(4, 7,  "perfect fifth",      "P5");
+		MINOR_SIXTH        = new SimpleInterval(5, 8,  "minor sixth",        "m6");
+		MAJOR_SIXTH        = new SimpleInterval(5, 9,  "major sixth",        "M6");
+		MINOR_SEVENTH      = new SimpleInterval(6, 10, "minor seventh",      "m7");
+		MAJOR_SEVENTH      = new SimpleInterval(6, 11, "major seventh",      "M7");
+		PERFECT_OCTAVE     = new SimpleInterval(7, 12, "perfect octave",     "P8");
 
-	
+		AUGMENTED_UNISON   = new SimpleInterval(0, 1,  "augmented unison",   "A1");
+		DIMINISHED_SECOND  = new SimpleInterval(1, 0,  "diminished second",  "d2");
+		AUGMENTED_SECOND   = new SimpleInterval(1, 3,  "augmented second",   "A2");
+		DIMINISHED_THIRD   = new SimpleInterval(2, 2,  "diminished third",   "d3");
+		AUGMENTED_THIRD    = new SimpleInterval(2, 5,  "augmented third",    "A3");
+		DIMINISHED_FOURTH  = new SimpleInterval(3, 4,  "diminished fourth",  "d4");
+		AUGMENTED_FOURTH   = new SimpleInterval(3, 6,  "augmented fourth",   "A4");
+		DIMINISHED_FIFTH   = new SimpleInterval(4, 6,  "diminished fifth",   "d5");
+		AUGMENTED_FIFTH    = new SimpleInterval(4, 8,  "augmented fifth",    "A5");
+		DIMINISHED_SIXTH   = new SimpleInterval(5, 7,  "diminished sixth",   "d6");
+		AUGMENTED_SIXTH    = new SimpleInterval(5, 10, "augmented sixth",    "A6");
+		DIMINISHED_SEVENTH = new SimpleInterval(6, 9,  "diminished seventh", "d7");
+		AUGMENTED_SEVENTH  = new SimpleInterval(6, 12, "augmented seventh",  "A7");
+		DIMINISHED_OCTAVE  = new SimpleInterval(7, 11, "diminished octave",  "d8");
+		AUGMENTED_OCTAVE   = new SimpleInterval(7, 13, "augmented octave",   "A8");
+	}
+	private static final List<SimpleInterval> values = Arrays.asList(
+			UNISON, MINOR_SECOND, MAJOR_SECOND,
+			MINOR_THIRD, MAJOR_THIRD, PERFECT_FOURTH, PERFECT_FIFTH,
+			MINOR_SIXTH, MAJOR_SIXTH, MINOR_SEVENTH, MAJOR_SEVENTH,
+			PERFECT_OCTAVE,
+			AUGMENTED_UNISON,
+			DIMINISHED_SECOND, AUGMENTED_SECOND,
+			DIMINISHED_THIRD, AUGMENTED_THIRD,
+			DIMINISHED_FOURTH, AUGMENTED_FOURTH,
+			DIMINISHED_FIFTH, AUGMENTED_FIFTH,
+			DIMINISHED_SIXTH, AUGMENTED_SIXTH,
+			DIMINISHED_SEVENTH, AUGMENTED_SEVENTH,
+			DIMINISHED_OCTAVE, AUGMENTED_OCTAVE
+	);
+
 	private final int degrees;
 	private final int semitones;
 	private final String name;
 	private final String symbol;
-	
+
 	/**
 	 * @param degrees the number of degree steps spanned
 	 * @param width the width in semitones
@@ -81,7 +111,7 @@ public enum SimpleInterval implements Interval {
 	public String symbol() {
 		return symbol;
 	}
-	
+
 	@Override
 	public boolean isEnharmonicWith(Interval other) {
 		if (other == null) {
@@ -94,7 +124,7 @@ public enum SimpleInterval implements Interval {
 	public String toString() {
 		return symbol;
 	}
-	
+
 	/**
 	 * Returns the simple interval which is composed of the given number
 	 * of diatonic degrees and has the given width in semitones.
@@ -112,9 +142,9 @@ public enum SimpleInterval implements Interval {
 		if (degrees < 0 || degrees > 7) {
 			return null;
 		}
-		
+
 		// Search the available constants
-		for (SimpleInterval i : values()) {
+		for (SimpleInterval i : values) {
 			if (i.degrees == degrees && i.semitones == semitones) {
 				return i;
 			}
