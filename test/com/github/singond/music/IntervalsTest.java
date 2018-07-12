450ae4c50bba5ef59d076da8eadee3a481e8939e
Jan Singon Slany <slanyj@centrum.cz>
2018-07-12 08:03:39 +0200

    Test creation of compound intervals

--- test/com/github/singond/music/IntervalsTest.java
+++ test/com/github/singond/music/IntervalsTest.java
@@ -1,8 +1,7 @@
 package com.github.singond.music;
 
-import static org.junit.Assert.assertEquals;
-
 import static com.github.singond.music.SimpleInterval.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -16,7 +15,7 @@ public class IntervalsTest {
 		simpleIntervalSearch(2, 4, SimpleInterval.MAJOR_THIRD);
 		simpleIntervalSearch(7, 12, SimpleInterval.PERFECT_OCTAVE);
 	}
-	
+
 	/**
 	 * @param d number of degrees
 	 * @param s width in semitones
@@ -26,7 +25,7 @@ public class IntervalsTest {
 		SimpleInterval actual = SimpleInterval.valueOf(d, s);
 		assertEquals("Searching interval failed.", exp, actual);
 	}
-	
+
 	@Test
 	public void simpleSums() {
 		sumTest(PERFECT_FIFTH, MAJOR_THIRD, MINOR_THIRD);
@@ -35,28 +34,48 @@ public class IntervalsTest {
 		sumTest(9, 16, PERFECT_OCTAVE, MAJOR_THIRD);
 		sumTest(10, 16, PERFECT_OCTAVE, MAJOR_THIRD, DIMINISHED_SECOND);
 	}
-	
+
 	private void sumTest(int expDeg, int expSteps, Interval a, Interval b) {
 		Interval sum = Intervals.sum(a, b);
 		assertEquals(expDeg, sum.degrees());
 		assertEquals(expSteps, sum.semitones());
 	}
-	
+
 	private void sumTest(int expDeg, int expSteps, Interval a, Interval b,
 	                     Interval... addends) {
 		Interval sum = Intervals.sum(a, b, addends);
 		assertEquals(expDeg, sum.degrees());
 		assertEquals(expSteps, sum.semitones());
 	}
-	
+
 	private void sumTest(Interval expected, Interval a, Interval b) {
 		Interval sum = Intervals.sum(a, b);
 		assertEquals(expected, sum);
 	}
-	
+
 	private void sumTest(Interval expected, Interval a, Interval b,
 	                     Interval... addends) {
 		Interval sum = Intervals.sum(a, b, addends);
 		assertEquals(expected, sum);
+	}
+
+	@Test
+	public void compoundCreation() {
+		createCompound(MINOR_THIRD, 1, 9, 15);
+		createCompound(PERFECT_FIFTH, 1, 11, 19);
+		createCompound(UNISON, 2, 14, 24);
+	}
+
+	private void createCompound(SimpleInterval simple, int octaves,
+	                            int degrees, int semitones) {
+		Interval fromSimple = CompoundInterval.of(simple, octaves);
+		assertEquals("Wrong number of degrees in compound interval ",
+				degrees, fromSimple.degrees());
+		assertEquals("Wrong number of semitones in compound interval ",
+				semitones, fromSimple.semitones());
+		Interval fromNumbers = CompoundInterval.of(degrees, semitones);
+		assertEquals("Wrong interval created by CompoundInterval.of(int, int)",
+				fromSimple, fromNumbers);
+		System.out.println(fromSimple);
 	}
 }
