fe960103b37c240effac778642d7936bada0608d
Jan Singon Slany <slanyj@centrum.cz>
2018-12-26 22:21:57 +0100

    Split TypedChord implementation into basic and invertible

--- src/main/java/com/github/singond/music/TypedChord.java
+++ src/main/java/com/github/singond/music/TypedChord.java
@@ -71,6 +71,26 @@ class TypedChord implements Chord {
 
 	/**
 	 * Creates a chord of the given type with the given pitch class as its
+	 * bass note.
+	 *
+	 * @param bass the bass note of the chord (as a {@code PitchClass})
+	 * @param type the type of chord to be created
+	 * @return a chord with the given {@code bass} and {@code type}
+	 */
+	public static final Invertible ofBass(PitchClass bass, InvertibleChordType type) {
+		if (bass == null) {
+			throw new NullPointerException("The chord bass is null");
+		} else if (type == null) {
+			throw new NullPointerException("The chord type is null");
+		}
+		// TODO Return preset constant if available
+		List<PitchClass> notes = fromBass(bass, type);
+		PitchClass root = notes.get(type.rootIndex());
+		return new Invertible(root, notes, type);
+	}
+
+	/**
+	 * Creates a chord of the given type with the given pitch class as its
 	 * root note.
 	 *
 	 * @param root the root note of the chord (as a {@code PitchClass})
@@ -86,6 +106,25 @@ class TypedChord implements Chord {
 		// TODO Return preset constant if available
 		List<PitchClass> notes = fromRoot(root, type);
 		return new TypedChord(root, notes, type);
+	}
+
+	/**
+	 * Creates a chord of the given type with the given pitch class as its
+	 * root note.
+	 *
+	 * @param root the root note of the chord (as a {@code PitchClass})
+	 * @param type the type of chord to be created
+	 * @return a chord with the given {@code root} and {@code type}
+	 */
+	public static final Invertible ofRoot(PitchClass root, InvertibleChordType type) {
+		if (root == null) {
+			throw new NullPointerException("The chord root is null");
+		} else if (type == null) {
+			throw new NullPointerException("The chord type is null");
+		}
+		// TODO Return preset constant if available
+		List<PitchClass> notes = fromRoot(root, type);
+		return new Invertible(root, notes, type);
 	}
 
 	private static List<PitchClass> fromBass(PitchClass bass, ChordType type) {
@@ -142,31 +181,8 @@ class TypedChord implements Chord {
 	}
 
 	@Override
-	public boolean invertible() {
-		return type.invertible();
-	}
-
-	@Override
-	public Chord invert(int n) {
-		if (type.inversion() == n) {
-			return this;
-		} else {
-			return ofRoot(root, type.invert(n));
-		}
-	}
-
-	@Override
 	public int inversion() {
 		return type.inversion();
-	}
-
-	@Override
-	public Chord rootPosition() {
-		if (type.inversion() == 0) {
-			return this;
-		} else {
-			return ofRoot(root, type.rootPosition());
-		}
 	}
 
 	@Override
@@ -182,5 +198,48 @@ class TypedChord implements Chord {
 	@Override
 	public String toString() {
 		return notes.toString();
+	}
+
+	private static class Invertible extends TypedChord
+			implements InvertibleChord {
+
+		private final PitchClass root;
+		private final InvertibleChordType type;
+
+		/**
+		 * Creates a new {@code TypedChord} with the given attributes.
+		 * This constructor directly stores the given references, meaning
+		 * it must stay private in order to ensure immutability.
+		 *
+		 * @param root the root note
+		 * @param notes the pitch classes of the chord in ascending order
+		 * @param type the chord type
+		 */
+		private Invertible(PitchClass root, List<PitchClass> notes,
+		                   InvertibleChordType type) {
+			super(root, notes, type);
+			this.root = root;
+			this.type = type;
+		}
+
+		@Override
+		public InvertibleChord invert(int n) {
+			if (type.inversion() == n) {
+				return this;
+			} else {
+				InvertibleChordType type = this.type.invert(n);
+				return new Invertible(root, fromRoot(root, type), type);
+			}
+		}
+
+		@Override
+		public InvertibleChord rootPosition() {
+			if (type.inversion() == 0) {
+				return this;
+			} else {
+				InvertibleChordType type = this.type.rootPosition();
+				return new Invertible(root, fromRoot(root, type), type);
+			}
+		}
 	}
 }
