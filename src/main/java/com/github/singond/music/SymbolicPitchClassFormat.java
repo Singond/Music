bca833f2ed7857e9344544382608ee363b1f0eb2
Jan Singon Slany <slanyj@centrum.cz>
2019-06-30 10:04:53 +0200

    Make accidental symbols a regular class

--- src/main/java/com/github/singond/music/SymbolicPitchClassFormat.java
+++ src/main/java/com/github/singond/music/SymbolicPitchClassFormat.java
@@ -56,23 +56,85 @@ class SymbolicPitchClassFormat implements PitchClassFormat {
 		}
 	}
 
-	public enum AccidentalSymbols {
-		ASCII("bb", "b", "", "#", "x"),
-		UNICODE("\uD834\uDD2B", "\u266D", "", "\u266F", "\uD834\uDD2A");
+	/**
+	 * A set of symbols to be used for representing sharps and flats.
+	 */
+	public static class AccidentalSymbols {
 
-		private String dblFlat;
-		private String flat;
-		private String natural;
-		private String sharp;
-		private String dblSharp;
+		public static final AccidentalSymbols ASCII = new AccidentalSymbols(
+				"bb", "b", "", "#", "x");
+		public static final AccidentalSymbols UNICODE = new AccidentalSymbols(
+				"\uD834\uDD2B", "\u266D", "", "\u266F", "\uD834\uDD2A");
 
-		AccidentalSymbols(String dblFlat, String flat,
+		private final String dblFlat;
+		private final String flat;
+		private final String natural;
+		private final String sharp;
+		private final String dblSharp;
+
+		/**
+		 * Constructs a new instance representing the given accidental symbols.
+		 * This specifies the symbols for natural, flat, sharp, double flat
+		 * and double sharp.
+		 *
+		 * @param dblFlat  the symbol for double flat pitch
+		 * @param flat     the symbol for flat pitch
+		 * @param natural  the symbol for natural pitch
+		 * @param sharp    the symbol for sharp pitch
+		 * @param dblSharp the symbol for double sharp pitch
+		 */
+		public AccidentalSymbols(String dblFlat, String flat,
 				String natural, String sharp,String dblSharp) {
 			this.dblFlat = dblFlat;
 			this.flat = flat;
 			this.natural = natural;
 			this.sharp = sharp;
 			this.dblSharp = dblSharp;
+		}
+
+		/**
+		 * Returns the symbol for double flat pitch.
+		 *
+		 * @return the double flat symbol
+		 */
+		public String getDoubleFlat() {
+			return dblFlat;
+		}
+
+		/**
+		 * Returns the symbol for flat pitch.
+		 *
+		 * @return the flat symbol
+		 */
+		public String getFlat() {
+			return flat;
+		}
+
+		/**
+		 * Returns the symbol for natural pitch.
+		 *
+		 * @return the natural symbol
+		 */
+		public String getNatural() {
+			return natural;
+		}
+
+		/**
+		 * Returns the symbol for sharp pitch.
+		 *
+		 * @return the sharp symbol
+		 */
+		public String getSharp() {
+			return sharp;
+		}
+
+		/**
+		 * Returns the symbol for double sharp pitch.
+		 *
+		 * @return the double sharp symbol
+		 */
+		public String getDoubleSharp() {
+			return dblSharp;
 		}
 	}
 }
